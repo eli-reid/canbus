c4b877804a0ef5b445a29187d23cd52b28843626
Eli Reid <elir@elireid.com>
2021-11-14 18:40:02 -0500

    touching items working

--- include/mainPage.h
+++ include/mainPage.h
@@ -10,20 +10,21 @@
 
 class mainPage: public Page
 {
-private:
-
 public:
         Menu menu;
         statusBar sb;
-        mainPage();
-        mainPage(Elegoo_GFX *screen);
+        void init(Elegoo_GFX* screen);
         ~mainPage();
 };
-mainPage::mainPage(){}
-mainPage::mainPage(Elegoo_GFX *screen)
-{       
+
+void mainPage::init(Elegoo_GFX *screen)
+{        
+        _itemIndex = 0;      
         sb=statusBar(screen,20, screen->width(),1,true,true,BOTTOM);
         menu =  Menu(screen,30,screen->width(),2,true,true,TOP);
+        
+        menu.setName("Main_Menu");
+        sb.setName("StatusBar");
         addItem(&menu);
         addItem(&sb);
         
