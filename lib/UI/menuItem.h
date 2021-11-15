9975d20097989edd1cbc3e09c9165cf3782b9160
Eli Reid <elir@elireid.com>
2021-11-14 19:13:45 -0500

    working on menyu items

--- lib/UI/menuItem.h
+++ lib/UI/menuItem.h
@@ -1,11 +1,21 @@
 #ifndef _MENUITEM_H
 #define _MENUITEM_H
+#include <string.h>
+#include <stdint.h>
+
 class menuItem
 {
 private:
-    /* data */
+    int _index;
+    int16_t _width;
+    int16_t _height;
+    String _title;
 public:
     menuItem(/* args */);
+    void setHeight(int16_t h);
+    int16_t getHeight();
+    
+
     ~menuItem();
 };
 
