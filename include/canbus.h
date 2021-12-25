1e7547b98920bca3bf4c471511c044d74f55673c
Eli Reid <elir@elireid.com>
2021-12-25 13:48:39 -0500

    adding can bus functionality

--- include/canbus.h
+++ include/canbus.h
@@ -9,9 +9,33 @@
 
 const int SPI_CS_PIN = 9;
 const int CAN_INT_PIN = 2;
+typedef struct BUFFER{
+    unsigned char len = 0;
+    unsigned char data[8];
+}buffer_T;
 
-class canBusInerface{
+class canBusInterface{
   private:
-   
+    MCP_CAN _can = MCP_CAN(SPI_CS_PIN);
+    bool _connected = false;
+    bool _msgFlag = false;
+    buffer_T _buffer;
+    void _onMsgRead();
+    void _onMsgRecv();
+  public:
+    //event methods
+    void(*onConnect)(void) = [](void){};
+    void(*onDisconnect)(void) = [](void){};
+    void(*onFail)(void) = [](void){};
+    void(*onMsgRecv)(void) = [](void){};
+    void(*onSendMsg)(void) = [](void){};
+    // methods
+    void connect();
+    void disconnect();
+    void msgCheck();
+    void msgRead();
+    //getters
+    buffer_T getBuffer(){return _buffer;}
+    bool isConnected(){return _connected;};
 };
 #endif
